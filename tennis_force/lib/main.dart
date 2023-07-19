a55d1eb68b2e0614fc8806126dc92a83e9eaf99d
Amany Abdelfattah <74238809+amany2010@users.noreply.github.com>
2023-07-20 01:51:46 +0300

    update lab assigments project name -add final project folder

---
+++ tennis_force/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:async';
+import 'package:testapp/motion.dart';
+
+import 'motion.dart';
+
+void main() {
+  //runApp(SensorsApp());
+  runApp(MyAp());
+}
