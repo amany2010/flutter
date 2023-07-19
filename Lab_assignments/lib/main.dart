39267442038a875d7dd49553bec79191ca004bb6
Amany Abdelfattah <74238809+amany2010@users.noreply.github.com>
2023-07-20 02:01:14 +0300

    update lab assigments project name -add final project folder

---
+++ Lab_assignments/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/birthDayCard.dart';
+import 'package:flutter_application_1/businessCard.dart';
+
+import 'basketballPointsCounter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of  application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      //home: MyHomePage(title: 'First Application'),
+      //home: businessCard(title: 'Business  Card'),
+      home: BasketballPointsCounter(title: 'Basketball Score Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Amany Abdelfattah',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
